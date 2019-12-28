c312c393c617ffb6ce44d7c5a5674898b443fc7e
MayJh4307 <norit.hunhan@hotmail.com>
2019-12-28 13:05:16 +0700

    lab3_3 ver1

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -2,26 +2,17 @@
 using namespace std;
 
 int main(){
-    char text1,text2,count ;
-    int N;
-    cout << "Enter the first text : ";
-    cin >> text1;
+    float a,b,c,ans;
+    a=1;
+    b=2;
+    c=a/b;
+    ans = 0;
 
-    cout << "Enter the second text : ";
-    cin >> text2;
-
-    cout << "Enter N : ";
-    cin >> N;
-
-    count = 0;
-
-    while (count < N){
-        if (count%2 == 0){
-            cout << text1 << " ";
-        }else{
-            cout << text2 << " ";
-        }
-        count = count + 1;
+    while(b<70){
+        cout << ans+c << " ";
+        ans = ans+c;
+        b=b+1;
     }
+    cout << "Ans = " << ans;
     return 0;
 }
